8be3e927a9bda48406fd2530ee751b382ac0da92
Amanda Sakuma, Danny Lucas <lannyducas@gmail.com>
2022-08-10 11:51:59 -0400

    commit tree 2a31c51cab7c19260607fffb932f90dc6dba8a42

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return(
+  <div id = "about">
+  <h2>About Me</h2>
+  <p>They call me birdman birdman junior bitch. I be high up in the sky shit on you and your bitch.</p>
+  <img src={image} alt = "I made this"></img>
+  </div>
+  );
 }
 
 export default About;
